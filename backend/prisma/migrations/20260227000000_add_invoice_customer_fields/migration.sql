95ab4a0b5c436d5a70b890183bc0152cb50f7a12
juan saavedra <juans0520@gmail.com>
2026-03-04 00:00:01 -0500

    quitar la migracion

--- backend/prisma/migrations/20260227000000_add_invoice_customer_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "dbDental"."Invoice" ADD COLUMN "subtotal" DOUBLE PRECISION,
-ADD COLUMN "igv" DOUBLE PRECISION,
-ADD COLUMN "customerName" TEXT,
-ADD COLUMN "documentType" TEXT,
-ADD COLUMN "documentId" TEXT,
-ADD COLUMN "address" TEXT,
-ADD COLUMN "email" TEXT;
